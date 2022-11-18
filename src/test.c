652044035c45cb7ab20116f1eb88352a879b6e94
Yasushi SHOJI <yashi@spacecubics.com>
2022-11-18 15:02:26 +0900

    Move test-trch.c to test.c
    It looks like PIC can't handle many function depth.  Flatten as much
    as possible.
    
    Signed-off-by: Yasushi SHOJI <yashi@spacecubics.com>

--- src/test.c
+++ src/test.c
@@ -1,14 +1,23 @@
 #include <stdbool.h>
 #include <stdio.h>
+#include <stdint.h>
 
-extern void do_test_trch(void);
-
-void do_tests(void)
-{
-        do_test_trch();
-}
+#include "trch.h"
 
 void expect(char *name, bool expect, bool val)
 {
-        printf("%s %s", name, expect == val ? "ok" : "ng");
+        printf("%s %s\n", name, expect == val ? "ok" : "ng");
+}
+
+void test_trch_r269(void)
+{
+        bool val = PORTCbits.RC2;
+
+        expect("r269", true, val);
+}
+
+void do_tests(void)
+{
+        /* TRCH */
+        test_trch_r269();
 }
