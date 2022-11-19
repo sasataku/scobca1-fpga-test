3dcec70256f5f8097ddfe2599b15e5533d6c1720
Takuya Sasaki <takuya.sasaki@spacecubics.com>
2022-11-19 20:29:36 +0900

    qspi: Kill warning
    This commit kill some build warning.
    
    Signed-off-by: Takuya Sasaki <takuya.sasaki@spacecubics.com>

--- src/qspi_common.c
+++ src/qspi_common.c
@@ -6,12 +6,13 @@
 
 #include "qspi_norflash_test.h"
 #include "qspi_fram_test.h"
+#include "common.h"
 
 uint32_t qspi_init(uint32_t test_no)
 {
 	uint32_t err_cnt = 0;
 
-	printk("* [%d] Start QSPI Initializing\n");
+	printk("* [%d] Start QSPI Initializing\n", test_no);
 
 	err_cnt += qspi_norflash_initialize(test_no);
 	err_cnt += qspi_fram_initialize(test_no);
