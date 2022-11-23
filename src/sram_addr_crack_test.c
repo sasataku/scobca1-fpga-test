916b67ec7f22cc6d88ed3ab5b27ddc9d7dbcb76c
Fumito Morishima <fumito@spacecubics.com>
2022-11-23 12:53:48 +0900

    Using mem_addr_t type and debug message funcs instead printk
    - use mem_addr_t type where address indicates instead of uint32_t
    - user debug() / info() functions instead of printk() functions

--- src/sram_addr_crack_test.c
+++ src/sram_addr_crack_test.c
@@ -9,15 +9,15 @@
 
 uint32_t sram_addr_crack_test(uint32_t test_no)
 {
-	printk("*** SRAM addr crack test starts ***\n");
+	info("*** SRAM addr crack test starts ***\n");
 	uint32_t err_num = 0;
 
-	const uint32_t sram_base_addr = 0x00200000; // avobe 2MByte
-	const uint32_t sram_last_addr = 0x00400000; // 2MByte
+	const mem_addr_t sram_base_addr = 0x00200000; // avobe 2MByte
+	const mem_addr_t sram_last_addr = 0x00400000; // 2MByte
 	const uint32_t sram_size = sram_last_addr - sram_base_addr;
 
 	// write different data to each address
-	uint32_t sram_addr = 0x00000000;
+	mem_addr_t sram_addr = 0x00000000;
 	write32(sram_addr + sram_base_addr, sram_addr);
 
 	for(sram_addr = 0x00000004;  sram_addr < sram_size;){
@@ -38,7 +38,7 @@ uint32_t sram_addr_crack_test(uint32_t test_no)
 		sram_addr = sram_addr << 1;
 	}
 
-	printk("*** test done, error count: %d ***\n", err_num);
+	info("*** test done, error count: %d ***\n", err_num);
 
 	return err_num;
 }
