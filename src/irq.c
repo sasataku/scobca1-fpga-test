6f1119f71e5c08e652d87260b123ff5f59e1ee75
Takuya Sasaki <takuya.sasaki@spacecubics.com>
2022-11-24 19:11:56 +0900

    irq: Add HRMEM/Config Memory IRQ handller
    This commit adds HRMEM/Config Memory(NOR Flash) IRQ handller.
    
    Signed-off-by: Takuya Sasaki <takuya.sasaki@spacecubics.com>

--- src/irq.c
+++ src/irq.c
@@ -7,16 +7,21 @@
 #include <zephyr/irq.h>
 #include "irq.h"
 #include "common.h"
-#include "i2c_test.h"
+#include "hrmem_test.h"
+#include "qspi_common.h"
 #include "can_test.h"
 
 #define IRQ_PRIO (2u)
 #define CAN_ISR_TXDONE_MASK (0x000000001)
 #define CAN_ISR_RXDONE_MASK (0x000000030)
 #define CAN_ISR_ERR_MASK    (0x000003FCE)
+#define QSPI_IER_ALL          (0x07070001)
+#define QSPI_ISR_SPIDONE_MASK (0x00000001)
+#define QSPI_ISR_ERR_MASK     (0x07070000)
 
 extern bool can_tx_done;
 extern bool can_rx_done;
+extern bool qspi_norflash_done;
 
 enum ScObcA1IrqNo {
 	IRQ_NO_UART = 0,
@@ -32,12 +37,29 @@ enum ScObcA1IrqNo {
 
 void hrmem_irq_cb(void *arg)
 {
-	printk("HRMEM IRQ\n");
+	uint32_t isr = sys_read32(SCOBCA1_FPGA_HRMEM_INTSTR);
+
+	/* HRMEM ISR is all error bit */
+	err("  !!! Assertion failed: Invalid HRMEM ISR: 0x%08x\n", isr);
+	write32(SCOBCA1_FPGA_HRMEM_INTSTR, isr);
 }
 
 void qspi_cfg_irq_cb(void *arg)
 {
-	printk("QSPI CFG IRQ\n");
+	uint32_t base = SCOBCA1_FPGA_CFG_BASE_ADDR;
+	uint32_t isr = sys_read32(SCOBCA1_FPGA_NORFLASH_QSPI_ISR(base));
+
+	debug("* QSPI CFG ISR 0x%08x\n", isr);
+	write32(SCOBCA1_FPGA_NORFLASH_QSPI_ISR(base), isr);
+
+	/* Check SPI DONE bit */
+	if ((isr & QSPI_ISR_SPIDONE_MASK) != 0) {
+		qspi_norflash_done = true;
+	}
+	/* Check error bit */
+	if ((isr & QSPI_ISR_ERR_MASK) != 0) {
+		err("  !!! Assertion failed: Invalid QSPI NOR Flash ISR: 0x%08x\n", isr);
+	}
 }
 
 void qspi_data_irq_cb(void *arg)
@@ -113,5 +135,10 @@ void irq_init(void) {
 	irq_enable(IRQ_NO_SYSMON);
 
 	/* Enable IER Ragister */
+	write32(SCOBCA1_FPGA_HRMEM_INTENR, 0x00000103);
+	write32(SCOBCA1_FPGA_NORFLASH_QSPI_IER(SCOBCA1_FPGA_CFG_BASE_ADDR), QSPI_IER_ALL);
 	write32(SCOBCA1_FPGA_CAN_IER, 0x00003fff);
+
+	/* Enable HRMEM Scrubing */
+	write32(SCOBCA1_FPGA_HRMEM_ECCCOLENR, 0x01);
 }
