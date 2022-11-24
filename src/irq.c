a0496b790889d740776d4a1d3b8265b180ebff84
Takuya Sasaki <takuya.sasaki@spacecubics.com>
2022-11-24 23:41:57 +0900

    irq: Add FRAM/System Monitor ISR handler
    This commit adds FRAM and System Monitor ISR handler.
    
    Signed-off-by: Takuya Sasaki <takuya.sasaki@spacecubics.com>

--- src/irq.c
+++ src/irq.c
@@ -10,18 +10,23 @@
 #include "hrmem_test.h"
 #include "qspi_common.h"
 #include "can_test.h"
+#include "system_monitor_reg.h"
 
 #define IRQ_PRIO (2u)
-#define CAN_ISR_TXDONE_MASK (0x000000001)
-#define CAN_ISR_RXDONE_MASK (0x000000030)
-#define CAN_ISR_ERR_MASK    (0x000003FCE)
+#define HRMEM_IER_ALL         (0x00000103)
+#define CAN_ISR_TXDONE_MASK   (0x00000001)
+#define CAN_ISR_RXDONE_MASK   (0x00000030)
+#define CAN_ISR_ERR_MASK      (0x00003FCE)
+#define CAN_IER_ALL           (0x00003fff)
 #define QSPI_IER_ALL          (0x07070001)
 #define QSPI_ISR_SPIDONE_MASK (0x00000001)
 #define QSPI_ISR_ERR_MASK     (0x07070000)
+#define SYSMON_IER_ALL        (0x00073f03)
 
 extern bool can_tx_done;
 extern bool can_rx_done;
 extern bool qspi_norflash_done;
+extern bool qspi_fram_done;
 extern bool first_can_err_isr;
 
 enum ScObcA1IrqNo {
@@ -59,18 +64,43 @@ void qspi_cfg_irq_cb(void *arg)
 	}
 	/* Check error bit */
 	if ((isr & QSPI_ISR_ERR_MASK) != 0) {
-		err("  !!! Assertion failed: Invalid QSPI NOR Flash ISR: 0x%08x\n", isr);
+		err("  !!! Assertion failed: Invalid QSPI NOR Flash (Config) ISR: 0x%08x\n", isr);
 	}
 }
 
 void qspi_data_irq_cb(void *arg)
 {
-	printk("QSPI DATA IRQ\n");
+	uint32_t base = SCOBCA1_FPGA_DATA_BASE_ADDR;
+	uint32_t isr = sys_read32(SCOBCA1_FPGA_NORFLASH_QSPI_ISR(base));
+
+	debug("* QSPI DATA ISR 0x%08x\n", isr);
+	write32(SCOBCA1_FPGA_NORFLASH_QSPI_ISR(base), isr);
+
+	/* Check SPI DONE bit */
+	if ((isr & QSPI_ISR_SPIDONE_MASK) != 0) {
+		qspi_norflash_done = true;
+	}
+	/* Check error bit */
+	if ((isr & QSPI_ISR_ERR_MASK) != 0) {
+		err("  !!! Assertion failed: Invalid QSPI NOR Flash (Data) ISR: 0x%08x\n", isr);
+	}
 }
 
 void qspi_fram_irq_cb(void *arg)
 {
-	printk("QSPI FRAM IRQ\n");
+	uint32_t isr = sys_read32(SCOBCA1_FPGA_FRAM_QSPI_ISR);
+
+	debug("* QSPI FRAM ISR 0x%08x\n", isr);
+	write32(SCOBCA1_FPGA_FRAM_QSPI_ISR, isr);
+
+	/* Check SPI DONE bit */
+	if ((isr & QSPI_ISR_SPIDONE_MASK) != 0) {
+		qspi_fram_done = true;
+	}
+	/* Check error bit */
+	if ((isr & QSPI_ISR_ERR_MASK) != 0) {
+		err("  !!! Assertion failed: Invalid QSPI NOR Flash (FRAM) ISR: 0x%08x\n", isr);
+	}
 }
 
 void can_irq_cb(void *arg)
@@ -97,19 +127,13 @@ void can_irq_cb(void *arg)
 	}
 }
 
-void internal_i2c_irq_cb(void *arg)
-{
-	printk("Internal I2C IRQ\n");
-}
-
-void external_i2c_irq_cb(void *arg)
-{
-	printk("External I2C IRQ\n");
-}
-
 void sysmon_irq_cb(void *arg)
 {
-	printk("Systemo Monitor IRQ\n");
+	uint32_t isr = sys_read32(SCOBCA1_FPGA_SYSMON_BHM_ISR);
+
+	/* System Monitor ISR is all error bit */
+	err("  !!! Assertion failed: Invalid System Monitor ISR: 0x%08x\n", isr);
+	write32(SCOBCA1_FPGA_SYSMON_BHM_ISR, isr);
 }
 
 void irq_init(void) {
@@ -129,19 +153,16 @@ void irq_init(void) {
 	IRQ_CONNECT(IRQ_NO_CAN, IRQ_PRIO, can_irq_cb, NULL, 0);
 	irq_enable(IRQ_NO_CAN);
 
-	IRQ_CONNECT(IRQ_NO_INTERNAL_I2C, IRQ_PRIO, internal_i2c_irq_cb, NULL, 0);
-	irq_enable(IRQ_NO_INTERNAL_I2C);
-
-	IRQ_CONNECT(IRQ_NO_EXTERNAL_I2C, IRQ_PRIO, external_i2c_irq_cb, NULL, 0);
-	irq_enable(IRQ_NO_EXTERNAL_I2C);
-
 	IRQ_CONNECT(IRQ_NO_SYSMON, IRQ_PRIO, sysmon_irq_cb, NULL, 0);
 	irq_enable(IRQ_NO_SYSMON);
 
 	/* Enable IER Ragister */
-	write32(SCOBCA1_FPGA_HRMEM_INTENR, 0x00000103);
+	write32(SCOBCA1_FPGA_HRMEM_INTENR, HRMEM_IER_ALL);
 	write32(SCOBCA1_FPGA_NORFLASH_QSPI_IER(SCOBCA1_FPGA_CFG_BASE_ADDR), QSPI_IER_ALL);
-	write32(SCOBCA1_FPGA_CAN_IER, 0x00003fff);
+	write32(SCOBCA1_FPGA_NORFLASH_QSPI_IER(SCOBCA1_FPGA_DATA_BASE_ADDR), QSPI_IER_ALL);
+	write32(SCOBCA1_FPGA_FRAM_QSPI_IER, QSPI_IER_ALL);
+	write32(SCOBCA1_FPGA_CAN_IER, CAN_IER_ALL);
+	write32(SCOBCA1_FPGA_SYSMON_BHM_IER, SYSMON_IER_ALL);
 
 	/* Enable HRMEM Scrubing */
 	write32(SCOBCA1_FPGA_HRMEM_ECCCOLENR, 0x01);
