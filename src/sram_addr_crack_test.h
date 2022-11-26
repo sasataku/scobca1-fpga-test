53e7fa16a06c6aa7934be0e384b6a7c1d9be1b48
Fumito Morishima <fumito@spacecubics.com>
2022-11-26 13:59:59 +0900

    Moidfy the method of testing for SRAM address and data.
    To make test method simple and leave comment for them to understand
    easier.

--- src/sram_addr_crack_test.h
+++ src/sram_addr_crack_test.h
@@ -7,6 +7,13 @@
 #ifndef SCOBCA1_FPGA_TEST_SRAM_ADDR_CRACK_H_
 #define SCOBCA1_FPGA_TEST_SRAM_ADDR_CRACK_H_
 
+/*
+ * use from 1M to 4MByte of SRAM is used for testing
+ * (make zephyr not to use this area )
+ */
+#define TEST_SRAM_START_ADDR 0x00100000
+#define TEST_SRAM_LAST_ADDR  0x003fffff
+
 uint32_t sram_addr_crack_test(uint32_t test_no);
 
 #endif /* SCOBCA1_FPGA_TEST_SRAM_ADDR_CRACK_H_ */
--- src/sram_data_crack_test.c
+++ src/sram_data_crack_test.c
@@ -6,26 +6,35 @@
 
 #include "common.h"
 #include "test_register.h"
+#include "sram_data_crack_test.h"
+
+#define TARGET_TEST_ADDR 0x00380000
 
 uint32_t sram_data_crack_test(uint32_t test_no)
 {
-    printk("*** SRAM data crack test starts ***\n");
-    uint32_t err_num = 0;
+	/*
+	 * test if each SRAM data connection works.
+	 * at first, set zero data to the SRAM address for testing
+	 * then toggles each data connection's signal level and
+	 * confirm it changes.
+	 */
 
-    volatile uint32_t target_mem = 0;
+    info("*** SRAM data crack test starts ***\n");
+    uint32_t err_count = 0;
 
-    if(target_mem){
-        err_num++;
+	write32(TARGET_TEST_ADDR, 0x0);
+    if(!assert32(TARGET_TEST_ADDR, 0x0, 0)){
+        err_count++;
     }
 
     for(int i = 0; i < 32 ; i++){
-        target_mem = 0x1 << i;
-        if(target_mem != 0x1 << i){
-        err_num++;
+        write32(TARGET_TEST_ADDR, 0x1 << i);
+        if(!assert32(TARGET_TEST_ADDR, 0x1 << i, 0)){
+            err_count++;
         }
     }
 
-    printk("*** test done, error count: %d ***\n", err_num);
+    info("*** test done, error count: %d ***\n", err_count);
 
-    return err_num;
+    return err_count;
 }
