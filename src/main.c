7b97e53ef965549ec41ca5e88476374f9ac7fca9
Yasushi SHOJI <yashi@spacecubics.com>
2022-11-26 09:47:07 +0900

    main: enable setters
    Enable PORT and TRIS setters.
    
    Signed-off-by: Yasushi SHOJI <yashi@spacecubics.com>

--- src/main.c
+++ src/main.c
@@ -214,4 +214,162 @@ void main(void)
 			break;
 		}
 	}
+        return v;
+}
+
+/* Port Get */
+uint8_t get_port_a(void) { return PORTA; }
+uint8_t get_port_b(void) { return PORTB; }
+uint8_t get_port_c(void) { return PORTC; }
+uint8_t get_port_d(void) { return PORTD; }
+uint8_t get_port_e(void) { return PORTE; }
+
+/* Port Set */
+void set_port_a(uint8_t val) { PORTA = val; }
+void set_port_b(uint8_t val) { PORTB = val; }
+void set_port_c(uint8_t val) { PORTC = val; }
+void set_port_d(uint8_t val) { PORTD = val; }
+void set_port_e(uint8_t val) { PORTE = val; }
+
+/* TRIS Get */
+uint8_t get_tris_a(void) { return TRISA; }
+uint8_t get_tris_b(void) { return TRISB; }
+uint8_t get_tris_c(void) { return TRISC; }
+uint8_t get_tris_d(void) { return TRISD; }
+uint8_t get_tris_e(void) { return TRISE; }
+
+/* TRIS Set */
+void set_tris_a(uint8_t val) { TRISA = val; }
+void set_tris_b(uint8_t val) { TRISB = val; }
+void set_tris_c(uint8_t val) { TRISC = val; }
+void set_tris_d(uint8_t val) { TRISD = val; }
+void set_tris_e(uint8_t val) { TRISE = val; }
+
+int spi_recv(uint8_t *buf, uint8_t len);
+void test_ok(bool ok);
+void test_send(uint8_t val);
+
+void do_active(void)
+{
+        int r;
+        uint8_t cmd[2];
+        bool ok;
+        uint8_t val;
+
+        r = spi_recv(cmd, 2);
+        if (r > 0) {
+                if (r == 1)
+                        printf("Got cmd %d 0x%x\n", cmd[0], cmd[0]);
+                if (r == 2)
+                        printf("Got cmd %d 0x%x arg %d 0x%x\n",
+                               cmd[0], cmd[0], cmd[1], cmd[1]);
+                switch (cmd[0]) {
+                case 0:
+                        ok = do_cmd1();
+                        test_ok(ok);
+                        break;
+                case 'a': val = get_port_a(); test_send(val); break;
+                case 'b': val = get_port_b(); test_send(val); break;
+                case 'c': val = get_port_c(); test_send(val); break;
+                case 'd': val = get_port_d(); test_send(val); break;
+                case 'e': val = get_port_e(); test_send(val); break;
+
+                case 'A': set_port_a(cmd[1]); test_ok(true); break;
+                case 'B': set_port_b(cmd[1]); test_ok(true); break;
+                case 'C': set_port_c(cmd[1]); test_ok(true); break;
+                case 'D': set_port_d(cmd[1]); test_ok(true); break;
+                case 'E': set_port_e(cmd[1]); test_ok(true); break;
+
+                case 't': val = get_tris_a(); test_send(val); break;
+                case 'u': val = get_tris_b(); test_send(val); break;
+                case 'v': val = get_tris_c(); test_send(val); break;
+                case 'w': val = get_tris_d(); test_send(val); break;
+                case 'x': val = get_tris_e(); test_send(val); break;
+
+                case 'T': set_tris_a(cmd[1]); test_ok(true); break;
+                case 'U': set_tris_b(cmd[1]); test_ok(true); break;
+                case 'V': set_tris_c(cmd[1]); test_ok(true); break;
+                case 'W': set_tris_d(cmd[1]); test_ok(true); break;
+                case 'X': set_tris_e(cmd[1]); test_ok(true); break;
+
+                default:
+                        test_ok(false);
+                        break;
+                }
+        }
+}
+
+void main (void)
+{
+        enum FpgaState fpga_state;
+        enum FpgaGoal activate_fpga = FPGA_SHUTDOWN;
+        int config_memory = 0;
+        int boot_mode = FPGA_BOOT_48MHZ;
+
+        // Initialize trch-firmware
+        trch_init();
+        fpga_program_maybe();
+        fpga_state = fpga_init();
+
+        usart_init();
+        timer2_init();
+        timer2_ctrl(1);
+        interrupt_enable();
+
+        /*
+         * Space Cubics OBC TRCH-Firmware Main
+         */
+        SRS3_UART_DE = 1;
+        SRS3_UART_RE_B = 0;
+        printf("SC OBC TRCH-FW for Quality Inspection\n");
+
+        do_tests();
+
+        while (1) {
+                if (FPGA_PWR_CYCLE_REQ) {
+                        activate_fpga = FPGA_SHUTDOWN;
+                }
+
+                fpga_state = fpga_state_control(activate_fpga, config_memory, boot_mode);
+                switch(fpga_state) {
+                case FPGA_STATE_POWER_DOWN:
+                        puts("Down");
+                        break;
+
+                case FPGA_STATE_POWER_OFF:
+                        puts("Off");
+                        activate_fpga = FPGA_ACTIVATE;
+                        break;
+
+                case FPGA_STATE_POWER_UP:
+                        puts("Up");
+                        break;
+
+                case FPGA_STATE_READY:
+                        puts("Ready");
+                        break;
+
+                case FPGA_STATE_CONFIG:
+                        puts("Config");
+                        __delay_ms(100);
+                        break;
+
+                case FPGA_STATE_ACTIVE:
+                        puts("Active");
+                        do_active();
+                        __delay_ms(500);
+                        break;
+
+                case FPGA_STATE_ERROR:
+                        puts("Error");
+                        activate_fpga = FPGA_SHUTDOWN;
+                        /* activate_fpga = FPGA_RECONFIGURE; */
+                        config_memory = !config_memory;
+                        break;
+
+                default:
+                        break;
+                }
+        }
+        return;
 }
