4fa8563aeb06d3a067fffc84faa4007af26db1cf
Fumito Morishima <fumito@spacecubics.com>
2022-11-26 13:59:59 +0900

    Moidfy the method of testing for SRAM address and data.
    To make test method simple and leave comment for them to understand
    easier.

--- src/sram_addr_crack_test.c
+++ src/sram_addr_crack_test.c
@@ -6,39 +6,66 @@
 
 #include "common.h"
 #include "test_register.h"
+#include "sram_addr_crack_test.h"
 
+/*
+ * This test checks if each address pin of SRAM works
+ */
 uint32_t sram_addr_crack_test(uint32_t test_no)
 {
+	/*
+	 * SRAM area for test is between 0x00100000 and 0x003fffff.
+	 * to check addr pin works, compare values between addresses
+	 * which only target pin's signal level is differ.
+	 *
+	 * In this case, while 0 to 18 pins, use addr 0x00200000 to
+	 * compare value, for 19 (highest) pin, use addr 0x00100000
+	 *
+	 * compare value with 0x00200000
+	 *  0: 0x00200004
+	 *  1: 0x00200008
+	 *  2: 0x00200010
+	 *  3: 0x00200020
+	 *  ....
+	 * 16: 0x00240000
+	 * 17: 0x00280000
+	 * 18: 0x00300000
+	 *
+	 * compare value with 0x00100000
+	 * 19: 0x00300000
+	 *
+	 * write different data to each address to compare
+	 * (using addr itself as different data)
+	 */
+
 	info("*** SRAM addr crack test starts ***\n");
-	uint32_t err_num = 0;
+	uint32_t err_count = 0;
 
-	const mem_addr_t sram_base_addr = 0x00200000; // avobe 2MByte
-	const mem_addr_t sram_last_addr = 0x00400000; // 2MByte
-	const uint32_t sram_size = sram_last_addr - sram_base_addr;
+	mem_addr_t test_addr;
 
-	// write different data to each address
-	mem_addr_t sram_addr = 0x00000000;
-	write32(sram_addr + sram_base_addr, sram_addr);
+	/* between 0 and 18 pins */
+	mem_addr_t compare_addr = 0x00200000;
+	write32(compare_addr, (uint32_t)compare_addr);
 
-	for(sram_addr = 0x00000004;  sram_addr < sram_size;){
-		write32((sram_addr + sram_base_addr), sram_addr);
-		sram_addr = sram_addr << 1;
-	}
-
-	// read data from each address and check if correct
-	sram_addr = 0x00000000;
-	if(read32(sram_addr + sram_base_addr) != sram_addr){
-		err_num++;
-	}
-
-	for(sram_addr = 0x00000004;  sram_addr < sram_size;){
-		if(read32(sram_addr + sram_base_addr) != sram_addr){
-			err_num++;
+	for(mem_addr_t i = 0x4; i <= 0x100000; i = i << 1){
+		test_addr = compare_addr + i;
+		write32(test_addr, test_addr);
+		if(read32(test_addr) == read32(compare_addr)){
+		    err_count++;
 		}
-		sram_addr = sram_addr << 1;
 	}
 
-	info("*** test done, error count: %d ***\n", err_num);
+	/* 19 pin */
+	compare_addr = 0x00100000;
+	write32(compare_addr, (uint32_t)compare_addr);
+	test_addr = 0x00300000;
+	write32(test_addr, (uint32_t)test_addr);
 
-	return err_num;
+	if(read32(test_addr) == read32(compare_addr)){
+	    err_count++;
+	}
+
+	info("*** test done, error count: %d ***\n", err_count);
+
+	return err_count;
 }
