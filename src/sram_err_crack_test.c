59fe1919c9e33b0b893fdfd282cb33dc3c60a4f8
Fumito Morishima <fumito@spacecubics.com>
2022-11-27 08:16:13 +0900

    Enable to check ECC error connection of SRAM1 and SRAM2 each.
    Change SRAM access from 32bit to 16bit to detect ECC error both of
     SRAM1 and SRAM2 expressly.

--- src/sram_err_crack_test.c
+++ src/sram_err_crack_test.c
@@ -16,14 +16,16 @@
 /* set SRAM address where never access before */
 #define TEST_START_ADDR 0x603F0000
 #define TEST_SKIP_ADDR 0x10
+#define HALFWORD_OFFSET 0x2
 
 uint32_t sram_err_crack_test(uint32_t test_no)
 {
-	/* 
+	/*
 	 * Test to check if ECC error signal connection is OK.
 	 * Clear ECC error counter at first, then read some SRAM addr
 	 * where never accessed after boot. It causes ECC error.
-	 * Ckeck if error count is increasing
+	 * Ckeck if error count increases
+	 * To see SRAM1 and 2 error each, use 16bit access
 	 */
 
 	info("*** SRAM ECC err crack test starts ***\n");
@@ -38,19 +40,45 @@ uint32_t sram_err_crack_test(uint32_t test_no)
 		info("Couldn't clear ECC error counter, %d\n",
 				read32(ECC1ERR_CNTR_REG));
 		err_count++;
+	    info("*** test done, error count: %u ***\n", err_count);
+		return err_count;
 	}
-	else{
-		mem_addr_t test_addr = TEST_START_ADDR;
-		int test_count = 0;
-		for(; test_count < max_test_count; test_count++){
-			read32(test_addr);
-			if(read32(ECC1ERR_CNTR_REG)){
-				break;
-			}
-			test_addr += TEST_SKIP_ADDR;
+
+	// Test lower addr
+	mem_addr_t test_addr = TEST_START_ADDR;
+	int test_count = 0;
+	for(; test_count < max_test_count; test_count++){
+		sys_read16(test_addr);
+		if(read32(ECC1ERR_CNTR_REG)){
+			break;
 		}
-		if(test_count == max_test_count) err_count++;
+		test_addr += TEST_SKIP_ADDR;
 	}
+	if(test_count == max_test_count) err_count++;
+
+	// clear ECC error count
+	write32(ECCERRCNTCLRR_REG, 0x1);
+
+	if(read32(ECC1ERR_CNTR_REG)){
+		// couldn't reset the counter
+		info("Couldn't clear ECC error counter, %d\n",
+				read32(ECC1ERR_CNTR_REG));
+		err_count++;
+	    info("*** test done, error count: %u ***\n", err_count);
+		return err_count;
+	}
+
+	// Test higher addr
+	test_addr = TEST_START_ADDR;
+	test_count = 0;
+	for(; test_count < max_test_count; test_count++){
+		sys_read16(test_addr + HALFWORD_OFFSET);
+		if(read32(ECC1ERR_CNTR_REG)){
+			break;
+		}
+		test_addr += TEST_SKIP_ADDR;
+	}
+	if(test_count == max_test_count) err_count++;
 
 	info("*** test done, error count: %u ***\n", err_count);
 
