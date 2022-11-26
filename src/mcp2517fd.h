0fce0b1f75b4b892b94f870641befe926d0f5dad
Yasushi SHOJI <yashi@spacecubics.com>
2022-11-26 09:23:17 +0900

    mcp2517fd.h: Add missing header file
    The commit 11f03178f4b includes mcp2517fd.h but it was not commited.
    
    Signed-off-by: Yasushi SHOJI <yashi@spacecubics.com>

---
+++ src/mcp2517fd.h
@@ -0,0 +1,25 @@
+/*
+ * Copyright (c) 2022 Space Cubics, LLC.
+ *
+ */
+
+#pragma once
+
+#define C1CON      (0x000)
+#define C1NBTCFG   (0x004)
+#define C1DBTCFG   (0x008)
+#define C1TDC      (0x00c)
+#define C1TBC      (0x010)
+
+#define C1FIFOCON1 (0x05c)
+#define C1FIFOSTA1 (0x060)
+#define C1FIFOUA1  (0x064)
+
+#define C1FIFOCON2 (0x068)
+#define C1FIFOSTA2 (0x06c)
+#define C1FIFOUA2  (0x070)
+
+#define C1FLTCON0  (0x1D0)
+
+#define C1FLTOBJ0  (0x1F0)
+#define C1MASK0    (0x1F4)
