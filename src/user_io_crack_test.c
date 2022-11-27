b2973637cf754c5ca33899449e84c21087ddfce6
Fumito Morishima <fumito@spacecubics.com>
2022-11-27 11:22:48 +0900

    Update data struct and init gpio mode to test loopback pins
    * Prepare the data structure for paird loopback pins and remove
     similar one for USER IO to replace.
    * Update loopback test func using the data structure
    * Update initial GPIO mode same as default mode to test USER IO.

--- src/user_io_crack_test.c
+++ src/user_io_crack_test.c
@@ -10,52 +10,46 @@
 /*
  * make USER IO pins pair to test
  */
-static struct user_io_test_regs user_io_pairs[] =
+static struct loopback_test_regs user_io_pairs[] =
 {
     // INPUT: UIO2, OUTPUT: UIO1
-    { TEST_CTRL_UIO2_00, TEST_CTRL_UIO1_00 ,TEST_MONI_USER_IO1, MONI_BIT_UIO1_00 },
-    { TEST_CTRL_UIO2_01, TEST_CTRL_UIO1_01 ,TEST_MONI_USER_IO1, MONI_BIT_UIO1_01 },
-    { TEST_CTRL_UIO2_02, TEST_CTRL_UIO1_02 ,TEST_MONI_USER_IO1, MONI_BIT_UIO1_02 },
-    { TEST_CTRL_UIO2_03, TEST_CTRL_UIO1_03 ,TEST_MONI_USER_IO1, MONI_BIT_UIO1_03 },
-    { TEST_CTRL_UIO2_04, TEST_CTRL_UIO1_04 ,TEST_MONI_USER_IO1, MONI_BIT_UIO1_04 },
-    { TEST_CTRL_UIO2_05, TEST_CTRL_UIO1_05 ,TEST_MONI_USER_IO1, MONI_BIT_UIO1_05 },
-    { TEST_CTRL_UIO2_06, TEST_CTRL_UIO1_06 ,TEST_MONI_USER_IO1, MONI_BIT_UIO1_06 },
-    { TEST_CTRL_UIO2_07, TEST_CTRL_UIO1_07 ,TEST_MONI_USER_IO1, MONI_BIT_UIO1_07 },
-    { TEST_CTRL_UIO2_08, TEST_CTRL_UIO1_08 ,TEST_MONI_USER_IO1, MONI_BIT_UIO1_08 },
-    { TEST_CTRL_UIO2_09, TEST_CTRL_UIO1_09 ,TEST_MONI_USER_IO1, MONI_BIT_UIO1_09 },
-    { TEST_CTRL_UIO2_10, TEST_CTRL_UIO1_10 ,TEST_MONI_USER_IO1, MONI_BIT_UIO1_10 },
-    { TEST_CTRL_UIO2_11, TEST_CTRL_UIO1_11 ,TEST_MONI_USER_IO1, MONI_BIT_UIO1_11 },
-    { TEST_CTRL_UIO2_12, TEST_CTRL_UIO1_12 ,TEST_MONI_USER_IO1, MONI_BIT_UIO1_12 },
-    { TEST_CTRL_UIO2_13, TEST_CTRL_UIO1_13 ,TEST_MONI_USER_IO1, MONI_BIT_UIO1_13 },
-    { TEST_CTRL_UIO2_14, TEST_CTRL_UIO1_14 ,TEST_MONI_USER_IO1, MONI_BIT_UIO1_14 },
-    { TEST_CTRL_UIO2_15, TEST_CTRL_UIO1_15 ,TEST_MONI_USER_IO1, MONI_BIT_UIO1_15 },
+    { TEST_CTRL_UIO2_00, TEST_CTRL_UIO1_00 ,TEST_MONI_USER_IO2, MONI_BIT_UIO2_00 },
+    { TEST_CTRL_UIO2_01, TEST_CTRL_UIO1_01 ,TEST_MONI_USER_IO2, MONI_BIT_UIO2_01 },
+    { TEST_CTRL_UIO2_02, TEST_CTRL_UIO1_02 ,TEST_MONI_USER_IO2, MONI_BIT_UIO2_02 },
+    { TEST_CTRL_UIO2_03, TEST_CTRL_UIO1_03 ,TEST_MONI_USER_IO2, MONI_BIT_UIO2_03 },
+    { TEST_CTRL_UIO2_04, TEST_CTRL_UIO1_04 ,TEST_MONI_USER_IO2, MONI_BIT_UIO2_04 },
+    { TEST_CTRL_UIO2_05, TEST_CTRL_UIO1_05 ,TEST_MONI_USER_IO2, MONI_BIT_UIO2_05 },
+    { TEST_CTRL_UIO2_06, TEST_CTRL_UIO1_06 ,TEST_MONI_USER_IO2, MONI_BIT_UIO2_06 },
+    { TEST_CTRL_UIO2_07, TEST_CTRL_UIO1_07 ,TEST_MONI_USER_IO2, MONI_BIT_UIO2_07 },
+    { TEST_CTRL_UIO2_08, TEST_CTRL_UIO1_08 ,TEST_MONI_USER_IO2, MONI_BIT_UIO2_08 },
+    { TEST_CTRL_UIO2_09, TEST_CTRL_UIO1_09 ,TEST_MONI_USER_IO2, MONI_BIT_UIO2_09 },
+    { TEST_CTRL_UIO2_10, TEST_CTRL_UIO1_10 ,TEST_MONI_USER_IO2, MONI_BIT_UIO2_10 },
+    { TEST_CTRL_UIO2_11, TEST_CTRL_UIO1_11 ,TEST_MONI_USER_IO2, MONI_BIT_UIO2_11 },
+    { TEST_CTRL_UIO2_12, TEST_CTRL_UIO1_12 ,TEST_MONI_USER_IO2, MONI_BIT_UIO2_12 },
+    { TEST_CTRL_UIO2_13, TEST_CTRL_UIO1_13 ,TEST_MONI_USER_IO2, MONI_BIT_UIO2_13 },
+    { TEST_CTRL_UIO2_14, TEST_CTRL_UIO1_14 ,TEST_MONI_USER_IO2, MONI_BIT_UIO2_14 },
+    { TEST_CTRL_UIO2_15, TEST_CTRL_UIO1_15 ,TEST_MONI_USER_IO2, MONI_BIT_UIO2_15 },
 };
 
 uint32_t user_io_crack_test(uint32_t test_no)
 {
-	/*
-	 * Test if external lines connection. Connect any two lines outside of the
-	 * board, Then control signal level of one line and see if it changes by the
-	 * other one.
-	 */
+    /*
+     * Test if external lines connection. Connect any two lines outside of the
+     * board, Then control signal level of one line and see if it changes by the
+     * other one.
+     */
 
-	info("*** User IO crack test starts ***\n");
+    info("*** User IO crack test starts ***\n");
     uint32_t err_count = 0;
 
     /* control UIO2 and use UIO1 as an input (default) */
     uint32_t test_item_num =
-           	sizeof(user_io_pairs) / sizeof(struct user_io_test_regs);
+               sizeof(user_io_pairs) / sizeof(struct loopback_test_regs);
 
     for(int i = 0; i < test_item_num; i++){
-        struct user_io_test_regs *pair = &user_io_pairs[i];
-        err_count += test_paired_pins_connection(
-						pair->in_ctrl_reg,
-						pair->out_ctrl_reg,
-						pair->in_moni_reg,
-						pair->moni_bitpos
-						);
+        err_count += test_paired_pins_connection(&user_io_pairs[i]);
     }
-	info("*** test done, error count: %d ***\n", err_count);
+    info("*** test done, error count: %d ***\n", err_count);
 
     return err_count;
 }
--- src/user_io_crack_test.h
+++ src/user_io_crack_test.h
@@ -9,14 +9,6 @@
 
 #include "test_register.h"
 
-struct user_io_test_regs
-{
-	uint32_t in_ctrl_reg;
-	uint32_t out_ctrl_reg;
-	uint32_t in_moni_reg;
-	uint8_t moni_bitpos;
-};
-
 uint32_t user_io_crack_test(uint32_t test_no);
 
 #endif /*  SCOBCA1_FPGA_TEST_USER_IO_CRACK_H_ */
