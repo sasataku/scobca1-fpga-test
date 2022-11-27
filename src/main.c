d997e333eaab9121c30fd2126d6fc7b914da760e
Fumito Morishima <fumito@spacecubics.com>
2022-11-27 16:13:35 +0900

    Adding QSPI bridge test function

--- src/main.c
+++ src/main.c
@@ -25,6 +25,7 @@
 #include "sram_err_crack_test.h"
 #include "sram_data_crack_test.h"
 #include "user_io_bridge_test.h"
+#include "qspi_bridge_test.h"
 #include "can_test.h"
 #include "bhm_test.h"
 #include "system_reg.h"
@@ -56,6 +57,7 @@ enum ScTestNo {
 	SC_TEST_CRACK_SRAM_ERR,
 	SC_TEST_CRACK_SRAM_DATA,
 	SC_TEST_BRIDGE_USER_IO,
+	SC_TEST_BRIDGE_QSPI,
 	SC_TEST_TRCH,
 	SC_TEST_CRACK_CAN,
 	SC_TEST_CRACK_I2C_INTERNAL,
@@ -92,6 +94,7 @@ void print_menu(void)
 	info("[%d] SRAM err crack Test\n", SC_TEST_CRACK_SRAM_ERR);
 	info("[%d] SRAM data crack Test\n", SC_TEST_CRACK_SRAM_DATA);
 	info("[%d] User IO Bridge Test\n", SC_TEST_BRIDGE_USER_IO);
+	info("[%d] QSPI Bridge Test\n", SC_TEST_BRIDGE_QSPI);
 
 	info("[%d] Shared Pin Test\n", SC_TEST_TRCH);
 	info("[%d] CAN crack Test\n", SC_TEST_CRACK_CAN);
@@ -193,6 +196,9 @@ void main(void)
 			break;
 		case SC_TEST_BRIDGE_USER_IO:
 			user_io_bridge_test(test_no);
+			break;
+		case SC_TEST_BRIDGE_QSPI:
+			qspi_bridge_test(test_no);
 			break;
 		case SC_TEST_CRACK_USB:
 			usb_crack_test(test_no);
--- src/user_io_bridge_test.h
+++ src/user_io_bridge_test.h
@@ -7,7 +7,7 @@
 #ifndef SCOBCA1_FPGA_TEST_USER_IO_BRIDGE_H_
 #define SCOBCA1_FPGA_TEST_USER_IO_BRIDGE_H_
 
-#include "user_io_crack_test.h"
+#include "test_register.h"
 
 uint32_t user_io_bridge_test(uint32_t test_no);
 
