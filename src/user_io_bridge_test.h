d3caf87c9d85c92fae551793c37da3ead69c323c
Fumito Morishima <fumito@spacecubics.com>
2022-11-27 11:32:10 +0900

    Adding user IO bridge test and removing previous bridge test module.

---
+++ src/user_io_bridge_test.h
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2022 Space Cubics, LLC.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#ifndef SCOBCA1_FPGA_TEST_USER_IO_BRIDGE_H_
+#define SCOBCA1_FPGA_TEST_USER_IO_BRIDGE_H_
+
+#include "user_io_crack_test.h"
+
+uint32_t user_io_bridge_test(uint32_t test_no);
+
+#endif /* SCOBCA1_FPGA_TEST_USER_IO_BRIDGE_H_ */
