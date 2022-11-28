9728d75e780e55658b4c33d7d3d2410da63fa83b
Takuya Sasaki <takuya.sasaki@spacecubics.com>
2022-11-28 23:24:44 +0900

    pdi: Update test contents
    This commit updtes PDI test contents
    
    Signed-off-by: Takuya Sasaki <takuya.sasaki@spacecubics.com>

--- src/pdi.c
+++ src/pdi.c
@@ -9,6 +9,7 @@
 #include "qspi_norflash_test.h"
 #include "qspi_fram_test.h"
 #include "bhm_test.h"
+#include "can_test.h"
 #include "can.h"
 
 void start_pdi(uint32_t test_no)
@@ -18,10 +19,22 @@ void start_pdi(uint32_t test_no)
 	/* Disable HRMEM IRQ */
 	irq_disable(IRQ_NO_HRMEM);
 
+	/* Wait a one seconds */
+	k_sleep(K_MSEC(1000));
+
 	info("* [%d] Start Pre Delivery Inspection\n", test_no);
 
+	info("* [%d-1] Start Initalize NOR flash/FRAM\n", no);
+	if (qspi_init(no) > 0) {
+		err("* [%d] !!! Abort Pre Delivery Inspection\n", test_no);
+		goto end_of_test;
+	}
+	no++;
 
-	info("* [%d-1] Start crack test\n", test_no);
+	/* Wait a one seconds */
+	k_sleep(K_MSEC(1000));
+
+	info("* [%d-2] Start crack test\n", test_no);
 	/* I2C Alert signal Test */
 /*
 	if (i2c_internal_crack_test(no) > 0) {
@@ -36,9 +49,9 @@ void start_pdi(uint32_t test_no)
 		goto end_of_test;
 	}
 
-	info("* [%d-2] Start bridge test\n", test_no);
+	info("* [%d-3] Start bridge test\n", test_no);
 
-	info("* [%d-3] Start functional test\n", test_no);
+	info("* [%d-4] Start functional test\n", test_no);
 
 	/* Config Memory Test */
 	if (qspi_config_memory_test(no) > 0) {
