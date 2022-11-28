be5dcedcf0e814bfdb890055dce8b3f150f69b08
Yasushi SHOJI <yashi@spacecubics.com>
2022-11-28 21:47:39 +0900

    usart: Remove unused code
    Signed-off-by: Yasushi SHOJI <yashi@spacecubics.com>

--- src/main.c
+++ src/main.c
@@ -66,7 +66,37 @@ enum ScTestNo {
 
 bool is_exit;
 
-void print_menu(void)
+// PIC16LF877A Configuration Bit Settings
+#pragma config FOSC = HS        // Oscillator Selection bits (HS oscillator)
+#pragma config WDTE = OFF       // Watchdog Timer Enable bit (WDT disabled)
+#pragma config PWRTE = OFF      // Power-up Timer Enable bit (PWRT enabled)
+#pragma config BOREN = OFF      // Brown-out Reset Enable bit (BOR enabled)
+#pragma config LVP = OFF        // Low-Voltage (Single-Supply) In-Circuit Serial Programming Enable bit (RB3/PGM pin has PGM function; low-voltage programming enabled)
+#pragma config CPD = OFF        // Data EEPROM Memory Code Protection bit (Data EEPROM code protection off)
+#pragma config WRT = OFF        // Flash Program Memory Write Enable bits (Write protection off; all program memory may be written to by EECON control)
+#pragma config CP = OFF         // Flash Program Memory Code Protection bit (Code protection off)
+
+static void __interrupt() isr(void) {
+        if (PIR1bits.TMR2IF) {
+                timer2_isr();
+        }
+}
+
+static void trch_init (void) {
+        ADCON1 = 0x07;
+        PORTA = PORTA_INIT;
+        TRISA = TRISA_INIT;
+        PORTB = PORTB_INIT;
+        TRISB = TRISB_INIT;
+        PORTC = PORTC_INIT;
+        TRISC = TRISC_INIT;
+        PORTD = PORTD_INIT;
+        TRISD = TRISD_INIT;
+        PORTE = PORTE_INIT;
+        TRISE = TRISE_INIT;
+}
+
+static void fpga_program_maybe(void)
 {
 	info("[%d] QSPI Initialize\n", SC_TEST_QSPI_INIT);
 	info("[%d] Long Run Test\n", SC_TEST_LONG_RUN);
