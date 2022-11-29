1cfbad4e2a939ad8bc9d9633ac5a5aef94ea3800
Yasushi SHOJI <yashi@spacecubics.com>
2022-11-29 15:02:24 +0900

    trch_test: Rename send_cmd_to_trch to can_send_cmd_to_trch
    I'd like to move send_cmd_to_trch() to can.c.  To do so I'm renaming
    the function as can.c requires.  can_test.c also has a function with
    the same name but this commit renames it to
    can_test_send_cmd_to_trch().
    
    Signed-off-by: Yasushi SHOJI <yashi@spacecubics.com>

--- src/trch_test.c
+++ src/trch_test.c
@@ -11,7 +11,7 @@
 #include "can.h"
 #include "test_register.h"
 
-int send_cmd_to_trch(uint8_t cmd, uint8_t arg, bool has_arg)
+int can_send_cmd_to_trch(uint8_t cmd, uint8_t arg, bool has_arg)
 {
 	uint16_t can_id = 'T';
 	uint8_t can_data[2];
@@ -83,29 +83,29 @@ int send_cmd_to_trch(uint8_t cmd, uint8_t arg, bool has_arg)
 
 #define FPGA_RESERVE        (7)
 
-uint8_t trch_get_porta(void) { return send_cmd_to_trch('a', 0, false); }
-uint8_t trch_get_portb(void) { return send_cmd_to_trch('b', 0, false); }
-uint8_t trch_get_portc(void) { return send_cmd_to_trch('c', 0, false); }
-uint8_t trch_get_portd(void) { return send_cmd_to_trch('d', 0, false); }
-uint8_t trch_get_porte(void) { return send_cmd_to_trch('e', 0, false); }
+uint8_t trch_get_porta(void) { return can_send_cmd_to_trch('a', 0, false); }
+uint8_t trch_get_portb(void) { return can_send_cmd_to_trch('b', 0, false); }
+uint8_t trch_get_portc(void) { return can_send_cmd_to_trch('c', 0, false); }
+uint8_t trch_get_portd(void) { return can_send_cmd_to_trch('d', 0, false); }
+uint8_t trch_get_porte(void) { return can_send_cmd_to_trch('e', 0, false); }
 
-uint8_t trch_get_trisa(void) { return send_cmd_to_trch('t', 0, false); }
-uint8_t trch_get_trisb(void) { return send_cmd_to_trch('u', 0, false); }
-uint8_t trch_get_trisc(void) { return send_cmd_to_trch('v', 0, false); }
-uint8_t trch_get_trisd(void) { return send_cmd_to_trch('w', 0, false); }
-uint8_t trch_get_trise(void) { return send_cmd_to_trch('x', 0, false); }
+uint8_t trch_get_trisa(void) { return can_send_cmd_to_trch('t', 0, false); }
+uint8_t trch_get_trisb(void) { return can_send_cmd_to_trch('u', 0, false); }
+uint8_t trch_get_trisc(void) { return can_send_cmd_to_trch('v', 0, false); }
+uint8_t trch_get_trisd(void) { return can_send_cmd_to_trch('w', 0, false); }
+uint8_t trch_get_trise(void) { return can_send_cmd_to_trch('x', 0, false); }
 
-uint8_t trch_set_porta(uint8_t val) { return send_cmd_to_trch('A', val, true); }
-uint8_t trch_set_portb(uint8_t val) { return send_cmd_to_trch('B', val, true); }
-uint8_t trch_set_portc(uint8_t val) { return send_cmd_to_trch('C', val, true); }
-uint8_t trch_set_portd(uint8_t val) { return send_cmd_to_trch('D', val, true); }
-uint8_t trch_set_porte(uint8_t val) { return send_cmd_to_trch('E', val, true); }
+uint8_t trch_set_porta(uint8_t val) { return can_send_cmd_to_trch('A', val, true); }
+uint8_t trch_set_portb(uint8_t val) { return can_send_cmd_to_trch('B', val, true); }
+uint8_t trch_set_portc(uint8_t val) { return can_send_cmd_to_trch('C', val, true); }
+uint8_t trch_set_portd(uint8_t val) { return can_send_cmd_to_trch('D', val, true); }
+uint8_t trch_set_porte(uint8_t val) { return can_send_cmd_to_trch('E', val, true); }
 
-uint8_t trch_set_trisa(uint8_t val) { return send_cmd_to_trch('T', val, true); }
-uint8_t trch_set_trisb(uint8_t val) { return send_cmd_to_trch('U', val, true); }
-uint8_t trch_set_trisc(uint8_t val) { return send_cmd_to_trch('V', val, true); }
-uint8_t trch_set_trisd(uint8_t val) { return send_cmd_to_trch('W', val, true); }
-uint8_t trch_set_trise(uint8_t val) { return send_cmd_to_trch('X', val, true); }
+uint8_t trch_set_trisa(uint8_t val) { return can_send_cmd_to_trch('T', val, true); }
+uint8_t trch_set_trisb(uint8_t val) { return can_send_cmd_to_trch('U', val, true); }
+uint8_t trch_set_trisc(uint8_t val) { return can_send_cmd_to_trch('V', val, true); }
+uint8_t trch_set_trisd(uint8_t val) { return can_send_cmd_to_trch('W', val, true); }
+uint8_t trch_set_trise(uint8_t val) { return can_send_cmd_to_trch('X', val, true); }
 
 uint8_t set_out (uint8_t val, uint8_t bit) { return val & ~(1 << bit); }
 uint8_t set_in  (uint8_t val, uint8_t bit) { return val |  (1 << bit); }
@@ -356,7 +356,7 @@ static uint32_t short_circuit_tests(void)
 
 uint32_t trch_test(void)
 {
-	int err_count = 0;
+	uint32_t err_count = 0;
 
 	info("* Start of Shared Pin Tests: Initializing\n");
 
