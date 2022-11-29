bc7f480f997e088b50cfe2220e462915b7f955e1
Yasushi SHOJI <yashi@spacecubics.com>
2022-11-29 15:26:03 +0900

    trch: Move common trch code to trch.h
    These functions can be used outside of trch_test.  Make it static
    inline and place them in newly created trch.h.
    
    Signed-off-by: Yasushi SHOJI <yashi@spacecubics.com>

---
+++ src/trch.h
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2022 Space Cubics, LLC.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#pragma once
+
+static inline uint8_t trch_get_porta(void) { return can_send_cmd_to_trch('a', 0, false); }
+static inline uint8_t trch_get_portb(void) { return can_send_cmd_to_trch('b', 0, false); }
+static inline uint8_t trch_get_portc(void) { return can_send_cmd_to_trch('c', 0, false); }
+static inline uint8_t trch_get_portd(void) { return can_send_cmd_to_trch('d', 0, false); }
+static inline uint8_t trch_get_porte(void) { return can_send_cmd_to_trch('e', 0, false); }
+
+static inline uint8_t trch_get_trisa(void) { return can_send_cmd_to_trch('t', 0, false); }
+static inline uint8_t trch_get_trisb(void) { return can_send_cmd_to_trch('u', 0, false); }
+static inline uint8_t trch_get_trisc(void) { return can_send_cmd_to_trch('v', 0, false); }
+static inline uint8_t trch_get_trisd(void) { return can_send_cmd_to_trch('w', 0, false); }
+static inline uint8_t trch_get_trise(void) { return can_send_cmd_to_trch('x', 0, false); }
+
+static inline uint8_t trch_set_porta(uint8_t val) { return can_send_cmd_to_trch('A', val, true); }
+static inline uint8_t trch_set_portb(uint8_t val) { return can_send_cmd_to_trch('B', val, true); }
+static inline uint8_t trch_set_portc(uint8_t val) { return can_send_cmd_to_trch('C', val, true); }
+static inline uint8_t trch_set_portd(uint8_t val) { return can_send_cmd_to_trch('D', val, true); }
+static inline uint8_t trch_set_porte(uint8_t val) { return can_send_cmd_to_trch('E', val, true); }
+
+static inline uint8_t trch_set_trisa(uint8_t val) { return can_send_cmd_to_trch('T', val, true); }
+static inline uint8_t trch_set_trisb(uint8_t val) { return can_send_cmd_to_trch('U', val, true); }
+static inline uint8_t trch_set_trisc(uint8_t val) { return can_send_cmd_to_trch('V', val, true); }
+static inline uint8_t trch_set_trisd(uint8_t val) { return can_send_cmd_to_trch('W', val, true); }
+static inline uint8_t trch_set_trise(uint8_t val) { return can_send_cmd_to_trch('X', val, true); }
--- src/trch_test.c
+++ src/trch_test.c
@@ -10,6 +10,7 @@
 #include "common.h"
 #include "can.h"
 #include "test_register.h"
+#include "trch.h"
 
 #define PORT_LEVEL_HIGH 1
 #define PORT_LEVEL_LOW 0
@@ -27,30 +28,6 @@
 #define FPGA_WATCHDOG       (4)
 
 #define FPGA_RESERVE        (7)
-
-uint8_t trch_get_porta(void) { return can_send_cmd_to_trch('a', 0, false); }
-uint8_t trch_get_portb(void) { return can_send_cmd_to_trch('b', 0, false); }
-uint8_t trch_get_portc(void) { return can_send_cmd_to_trch('c', 0, false); }
-uint8_t trch_get_portd(void) { return can_send_cmd_to_trch('d', 0, false); }
-uint8_t trch_get_porte(void) { return can_send_cmd_to_trch('e', 0, false); }
-
-uint8_t trch_get_trisa(void) { return can_send_cmd_to_trch('t', 0, false); }
-uint8_t trch_get_trisb(void) { return can_send_cmd_to_trch('u', 0, false); }
-uint8_t trch_get_trisc(void) { return can_send_cmd_to_trch('v', 0, false); }
-uint8_t trch_get_trisd(void) { return can_send_cmd_to_trch('w', 0, false); }
-uint8_t trch_get_trise(void) { return can_send_cmd_to_trch('x', 0, false); }
-
-uint8_t trch_set_porta(uint8_t val) { return can_send_cmd_to_trch('A', val, true); }
-uint8_t trch_set_portb(uint8_t val) { return can_send_cmd_to_trch('B', val, true); }
-uint8_t trch_set_portc(uint8_t val) { return can_send_cmd_to_trch('C', val, true); }
-uint8_t trch_set_portd(uint8_t val) { return can_send_cmd_to_trch('D', val, true); }
-uint8_t trch_set_porte(uint8_t val) { return can_send_cmd_to_trch('E', val, true); }
-
-uint8_t trch_set_trisa(uint8_t val) { return can_send_cmd_to_trch('T', val, true); }
-uint8_t trch_set_trisb(uint8_t val) { return can_send_cmd_to_trch('U', val, true); }
-uint8_t trch_set_trisc(uint8_t val) { return can_send_cmd_to_trch('V', val, true); }
-uint8_t trch_set_trisd(uint8_t val) { return can_send_cmd_to_trch('W', val, true); }
-uint8_t trch_set_trise(uint8_t val) { return can_send_cmd_to_trch('X', val, true); }
 
 uint8_t set_out (uint8_t val, uint8_t bit) { return val & ~(1 << bit); }
 uint8_t set_in  (uint8_t val, uint8_t bit) { return val |  (1 << bit); }
